92f3a3de3c85f2618d200db02a349b8670c14b73
Pablo Mellado <reelles@gmail.com>
2018-04-03 14:54:04 -0300

    Se Agregan vista de productos

---
+++ WebSysme/Controllers/ProductosController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebSysme.Controllers
+{
+    public class ProductosController : Controller
+    {
+        public IActionResult ProductsGrid()
+        {
+            return View();
+        }
+    }
+}
